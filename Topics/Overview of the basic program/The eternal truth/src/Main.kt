16013223b704299e5b8e84dc355b5e25a6a95491
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-05-02 15:11:16 +0600

    add hello world

--- Topics/Overview of the basic program/The eternal truth/src/Main.kt
+++ Topics/Overview of the basic program/The eternal truth/src/Main.kt
@@ -1,3 +1,3 @@
 fun main() {
-    TODO("Remove this line and write your solution here")
+    println("2 + 2 = 4")
 }
--- Topics/Overview of the basic program/Theory/src/Main.kt
+++ Topics/Overview of the basic program/Theory/src/Main.kt
@@ -1,5 +1,5 @@
 // You can experiment here, it won’t be checked
 
 fun main(args: Array<String>) {
-  // put your code here
+  println("Hello, World")
 }
