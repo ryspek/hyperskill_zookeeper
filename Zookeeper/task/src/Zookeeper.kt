55beffd6cf06a048b393fc2fca4c510970e0c88f
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-05-03 18:24:51 +0600

    finish first stage

---
+++ Zookeeper/task/src/Zookeeper.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("I love animals!")
+    println("Let's check on the animals...")
+    println("The deer looks fine.")
+    println("The bat looks happy.")
+    println("The lion looks healthy.")
+}
